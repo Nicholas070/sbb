0354239986c6783c0aa4b91a83505f743baae2d4
Nicholas070 <kid0807@naver.com>
2025-01-07 17:23:44 +0900

    “findBySubjectAndContent”

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -18,9 +18,9 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-		Question q = qList.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
+		Question q = this.questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(31, q.getId());
 	}
 }
 //테스트를 실행하시면 테스트만 끝내고, 서버는 실행되지 않습니다
