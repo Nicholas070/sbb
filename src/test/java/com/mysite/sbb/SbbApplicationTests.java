f35dccd6b989401144fce2b9bb2e2cb39beede20
Nicholas070 <kid0807@naver.com>
2025-01-07 17:21:26 +0900

    “findBySubjectLike”

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,11 +1,14 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -13,21 +16,12 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
-    //테스트를 실행하시면 테스트만 끝내고, 서버는 실행되지 않습니다
-	// 에스비비어플리케이션을 실행하셔야돼요
-
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
 	}
 }
+//테스트를 실행하시면 테스트만 끝내고, 서버는 실행되지 않습니다
+// 에스비비어플리케이션을 실행하셔야돼요
