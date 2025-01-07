699f3fca46e6f9977cbada78abac1f21df0c3589
Nicholas070 <kid0807@naver.com>
2025-01-07 17:25:04 +0900

    “findBySubject”

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -18,8 +18,7 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q = this.questionRepository.findBySubjectAndContent(
-				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
 		assertEquals(31, q.getId());
 	}
 }
